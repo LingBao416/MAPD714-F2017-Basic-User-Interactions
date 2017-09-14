bddb9cf89e54e1d0dcc7bd4753f51d66cdd724a8
Student <student@setass-mac-mini.local>
2017-09-13 22:25:24 -0400

    Left/Right botton

--- Basic-user-interactions/ViewController.swift
+++ Basic-user-interactions/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func bottonAction(_ sender: UIButton) {
+        print(sender.titleLabel!.text)
+        outputLabel.text = sender.titleLabel!.text! + "botton was clicked"
+    }
 
 }
 
